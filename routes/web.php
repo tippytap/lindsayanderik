7f340b2b9b4c56851dcad30ae82ecf09af8c1b21
Erik Miller <emiller46@radford.edu>
2019-02-24 13:57:13 -0500

    Fixed class name reference typo which caused server error

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/wedding/{id}', 'WeddingController@show')->name('weddingShow');
 
 Route::get('/invite/{wedding}', 'InviteeController@invite');
-Route::post('/create/{wedding}', 'Inviteecontroller@create')->name('createInvite');
+Route::post('/create/{wedding}', 'InviteeController@create')->name('createInvite');
 Route::get('/invitee/{wedding}/{id}', 'InviteeController@edit')->name('editInvitee');
 Route::post('/invitee/{weddint}/{id}', 'InviteeController@update');
