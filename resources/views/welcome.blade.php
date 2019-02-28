4429103bf5c08c2c99d2cc6aee2d16fe22718152
Erik Miller <emiller46@radford.edu>
2019-02-27 22:27:25 -0500

    Added wayfair registry Added hotel info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                         <a href="#rsvp">RSVP</a>
                     </li>
                     <li>
-                        <a href="#travel">Travel</a>
+                        <a href="#travel">Hotel</a>
                     </li>
                     <li>
                         <a href="#registry">Registry</a>
@@ -58,7 +58,7 @@
                     <div class="card layer-up col-md-12">
                         <div class="card-content">
                             <h3>Join Us!</h3>
-                            <p><strong>June 1, 2019</strong><br/>Sundara, Boones Mill, Virginia<br/><strong>5:00pm</strong></p>
+                            <p><strong>5:00pm</strong><br/>Sundara, Boones Mill, Virginia<br/><strong>June 1, 2019</strong></p>
                             <p>Come celebrate our marriage with us!</p>
                             <a class="btn btn-primary" href='{{ url("/invite/1") }}'>RSVP Here</a>
                             <div style="margin-top: 10px;">or email us at <a href="mailto:lindskey@gmail.com">lindskey@gmail.com</a></div>
@@ -72,13 +72,37 @@
                 </div>
             </div>
 
-            <div id="travel" class="content ">
-                <div class="card-container w-50 center">
+            <div id="travel" class="content">
+                <div class="photo-container paralaxThis left">
+                    <div class="photo">
+                        <img src="images/hiltongardeninn.jpg" alt="outdoor view of the Hilton Garden Inn">
+                    </div>
+                </div>
+                <div class="card-container w-50 right">
                     <div class="card layer-up">
                         <div class="card-content">
-                            <h3>Travel</h3>
-                            <p class="text-left">We have held rooms at a local hotel. We have also reserved a shuttle to carry you to the venue and back. Please check back to find details soon!</p>
-                            {{-- <a class="btn btn-primary" href="#">Book a room</a> --}}
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h4 class="text-left">
+                                        <strong style="font-size: 2em; text-transform: uppercase;">Hilton Garden Inn</strong><br>
+                                        4500 South Peak Boulevard, Roanoke, Virginia 24018
+                                    </h4>
+                                        <br>
+                                    <p class="text-left">
+                                        For your convenience, a block of hotel rooms has been reserved at the Hilton Garden Inn.
+                                    </p>
+                                    <p class="text-left">
+                                        You may make your reservation online by going to the link below or you may call the hotel directly to make your reservation. If you would prefer to call the hotel, please call <strong>540-776-3400</strong> and use the Group Code: <span style="color: red;">KEYMIL.</span>
+                                    </p>
+                                    <p class="text-left">
+                                        Be sure to book your hotel room by <span style="color: red; text-transform: uppercase; font-weight: 700;">May 1st</span> in order to receive the discount rate.
+                                    </p>
+                                    <p class="text-left">
+                                        There will also be a shuttle available from the hotel to Sundara which is approximately 15 minutes (10 miles) away. The shuttle will transport guests to the ceremony and return to the hotel after the reception. When RSVPing, please be sure to let us know if you plan to use the shuttle.
+                                    </p>
+                                </div>
+                            </div>
+                            <a class="btn btn-primary" href="https://hiltongardeninn.hilton.com/en/gi/groups/personalized/R/ROAGIGI-KEYMIL-20190531/index.jhtml?WT.mc_id=POG">Book a room</a>
                         </div>
                     </div>
                 </div>
@@ -86,7 +110,7 @@
 
             <div id="registry" class="content">
                 <div class="card-container row center">
-                    <div class="card layer-up col-md-6 col-xs-12">
+                    <div class="card layer-up w-100">
                         <div class="card-content">
                             <h3>Registeries</h3>
                             <br>
@@ -102,6 +126,12 @@
                                         <img src="images/honeyfund-white.svg" class="hide" alt="honeyfund logo">
                                     </a>
                                 </div>
+                                <div class="registry-block">
+                                    <a id="wayfair" href="#" class="registry-link" aria-label="Honeyfund registry link">
+                                        <img src="images/wayfair.svg" class="show" alt="wayfair logo">
+                                        <img src="images/wayfair-white.svg" class="hide" alt="wayfair logo">
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -112,17 +142,17 @@
         <script type="text/javascript">
         $(document).ready(function(){
             // $("#honeyfund").find('img').eq(1).hide();
-            $('#honeyfund').hover(function(){
-                var img = $(this).find('img');
-                img.each(function(){
-                    if($(this).hasClass('hide')){
-                        $(this).removeClass('hide').addClass('show');
-                    }
-                    else{
-                        $(this).removeClass('show').addClass('hide');
-                    }
-                })
-            });
+            // $('#honeyfund').hover(function(){
+            //     var img = $(this).find('img');
+            //     img.each(function(){
+            //         if($(this).hasClass('hide')){
+            //             $(this).removeClass('hide').addClass('show');
+            //         }
+            //         else{
+            //             $(this).removeClass('show').addClass('hide');
+            //         }
+            //     })
+            // });
         });
         </script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/brands.js" integrity="sha384-zJ8/qgGmKwL+kr/xmGA6s1oXK63ah5/1rHuILmZ44sO2Bbq1V3p3eRTkuGcivyhD" crossorigin="anonymous"></script>
